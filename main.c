f84ce5de50d412bae841538436fafec5a4dab80e
Greg Hope <33733534+greghope667@users.noreply.github.com>
2022-06-27 16:24:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stddef.h>
+
+const char* SECRET_KEY = "abcdef";
+
+size_t version (void)
+{
+	// FIXME: this function has many problems
+	size_t* out;
+	printf ("Version number equals: %s%n\n", SECRET_KEY, out);
+	return *out;
+}
 
 int main (int argc, char** argv)
 {
 	if (argc > 0)
 		// This is a bug - see if CI detects this
 		printf (argv);
+	
+	version();
 	return 0;
 }
